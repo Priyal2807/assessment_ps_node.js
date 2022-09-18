1dcab28dffaf4b019ae2d00598a1271445d2bf0f
Priyal2807 <priyalsobti@gmail.com>
2022-09-18 19:42:48 +0530

    changes in index.js as per instructions

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var url = require('url')
+const ytcm = require("@freetube/yt-comment-scraper")
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/api/scrape-sentiments', (req, res) => {
+    const yurl = req.query.url;
+    let q = url.parse(yurl, true);
+    const id = q.search;
+    const actualId = id.substring(3);
+    const payload = {
+        videoId: actualId, // Required
+        mustSetCookie: false
+    }
+    ytcm.getComments(payload).then((data) => {
+        res.status(201).send({
+            "id": actualId,
+            "comments": data.total,
+            "videoUrl":yurl
+        })
+    }).catch((error) => {
+        res.status(400).send({
+            "status": "failure",
+            "reason":"wrong url"
+        })
+    });
+
+})
 
 module.exports = router;
